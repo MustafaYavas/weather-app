c078b55a9e409a77e6f5460b35b66ff72fd14bcb
MustafaYavas <mustafayavas40@gmail.com>
2023-04-01 15:08:34 +0300

    Create project template

---
+++ app/[[...city]]/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HomeContainer from '../containers/home';
+
+const HomePage = () => {
+  return (
+    <div>
+      <HomeContainer />
+    </div>
+  );
+};
+
+export default HomePage;

29c9cd778a041d3ccdf20bf34874030fa8ff17da
MustafaYavas <mustafayavas40@gmail.com>
2023-04-02 17:31:21 +0300

    Update icon and font sizes

--- helpers/icons.js
+++ helpers/icons.js
@@ -13,25 +13,25 @@ import { TbWind } from 'react-icons/tb';
 
 const getWeatherIcon = (iconName, left) => {
   if (iconName === '01d') {
-    return <BsSun size={left ? 60 : 36} color="var(--second-color)" />;
+    return <BsSun size={left ? 88 : 48} color="var(--second-color)" />;
   } else if (iconName === '02d') {
-    return <BsCloudy size={left ? 60 : 36} color="var(--second-color)" />;
+    return <BsCloudy size={left ? 88 : 48} color="var(--second-color)" />;
   } else if (iconName === '03d' || iconName === '04d') {
-    return <BsClouds size={left ? 60 : 36} color="var(--second-color)" />;
+    return <BsClouds size={left ? 88 : 48} color="var(--second-color)" />;
   } else if (iconName === '09d') {
-    return <BsCloudRain size={left ? 60 : 36} color="var(--second-color)" />;
+    return <BsCloudRain size={left ? 88 : 48} color="var(--second-color)" />;
   } else if (iconName === '10d') {
     return (
-      <BsCloudRainHeavy size={left ? 60 : 36} color="var(--second-color)" />
+      <BsCloudRainHeavy size={left ? 88 : 48} color="var(--second-color)" />
     );
   } else if (iconName === '11d') {
     return (
-      <RiThunderstormsLine size={left ? 60 : 36} color="var(--second-color)" />
+      <RiThunderstormsLine size={left ? 88 : 48} color="var(--second-color)" />
     );
   } else if (iconName === '13d') {
-    return <BsCloudSnow size={left ? 60 : 36} color="var(--second-color)" />;
+    return <BsCloudSnow size={left ? 88 : 48} color="var(--second-color)" />;
   } else if (iconName === '50d') {
-    return <RiMistFill size={left ? 60 : 36} color="var(--second-color)" />;
+    return <RiMistFill size={left ? 88 : 48} color="var(--second-color)" />;
   } else if (iconName === 'rain') {
     return <BsUmbrella size={20} color="var(--main-text-dull)" />;
   } else if (iconName === 'wind') {

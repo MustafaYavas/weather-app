aef289a4e04a6313b9d83e54a5c789f4927de8ab
MustafaYavas <mustafayavas40@gmail.com>
2023-04-05 19:30:32 +0300

    Change services folder locaiton and update fetch function

--- app/[...city]/page.js
+++ app/[...city]/page.js
@@ -1,11 +1,10 @@
 import CityContainer from '../../containers/city';
-import { getWeatherData } from '../services/city';
+import { getWeatherData } from '../../services/city';
 import { notFound } from 'next/navigation';
 
 const CityPage = async ({ params }) => {
   let weatherInfo;
   let cityInfo;
-
   if (params?.city.length === 1) {
     const datas = await getWeatherData(params.city[0]);
     weatherInfo = datas?.weatherInfo;
--- services/city.js
+++ services/city.js
@@ -1,31 +1,36 @@
+const BASE_URL = 'https://api.openweathermap.org';
+const API = `appid=${process.env.API_KEY}`;
+
 const getWeatherData = async (city) => {
   try {
     const locResult = await fetch(
-      `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${process.env.API_KEY}`
+      `${BASE_URL}/geo/1.0/direct?q=${city}&limit=1&${API}`
     );
 
     if (!locResult.ok)
       throw new Error('An unknown error has been occured. Please try again!');
+
     const locDatas = await locResult.json();
     const { lat, lon } = locDatas[0];
 
     const weatherResult = await fetch(
-      `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${process.env.API_KEY}&units=metric`
+      `${BASE_URL}/data/2.5/forecast?lat=${lat}&lon=${lon}&${API}`
     );
+
     if (!weatherResult.ok)
       throw new Error('An unknown error has been occured. Please try again!');
+
     const weatherDatas = await weatherResult.json();
 
     let weatherInfo = [];
-    weatherDatas.list.map((data) => {
-      if (data.dt_txt.includes('12:00:00')) {
-        weatherInfo.push(data);
-      }
+    weatherDatas.list?.map((data) => {
+      if (data.dt_txt.includes('12:00:00')) weatherInfo.push(data);
     });
-    let cityInfo = weatherDatas.city;
 
-    return { weatherInfo, cityInfo };
-  } catch (error) {}
+    return { weatherInfo, cityInfo: weatherDatas.city };
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export { getWeatherData };
